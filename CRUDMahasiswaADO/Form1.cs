1a0367bf3d2320e1ab5788dfa0dbe4eb5b73e6b1
anneisza <rizaanneisza@gmail.com>
2026-04-10 11:16:14 +0700

    Mengatur tampilan awal form

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -277,6 +277,22 @@ namespace CRUDMahasiswaADO
             txtNIM.Focus();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbJK.Items.Clear();
+            cmbJK.Items.Add("L");
+            cmbJK.Items.Add("P");
+
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1.CellClick += dataGridView1_CellClick;
+
+        }
+
         private void label1_Click(object sender, EventArgs e)
         {
 
