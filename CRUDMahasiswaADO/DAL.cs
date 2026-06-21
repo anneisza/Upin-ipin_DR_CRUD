0b83dd3c122e183ec306bec225c741d4dc3d86b8
anneisza <rizaanneisza@gmail.com>
2026-06-21 19:24:27 +0700

    Add InsertMhs method

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -62,5 +62,44 @@ namespace CRUDMahasiswaADO
 
         }
 
+        public void InsertMhs(string nim, string nama, string alamat, string jenisKelamin, DateTime tanggalLahir, string kodeProdi, byte[] foto)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlTransaction trans = conn.BeginTransaction();
+            try
+            {
+
+                SqlCommand command = new SqlCommand("sp_InsertMahasiswa", conn, trans);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@pNIM", nim);
+                command.Parameters.AddWithValue("@pNama", nama);
+                command.Parameters.AddWithValue("@pAlamat", alamat);
+                command.Parameters.AddWithValue("@pTanggalLahir", tanggalLahir);
+                command.Parameters.AddWithValue("@pJenisKelamin", jenisKelamin);
+                command.Parameters.AddWithValue("@pKodeProdi", kodeProdi);
+                command.Parameters.AddWithValue("@pFoto", foto);
+                // Di DAL.cs InsertMhs() | Tambah sendiri
+                //command.Parameters.AddWithValue("@pFoto", foto != null ? (object)foto : DBNull.Value);
+
+                command.ExecuteNonQuery();
+                trans.Commit();
+            }
+            catch (Exception ex)
+            {
+                trans.Rollback();
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
     }
 }
