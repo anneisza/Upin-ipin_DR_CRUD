16c575312dddce6cb89b8d31ce7462dc0e3f0d44
anneisza <rizaanneisza@gmail.com>
2026-06-21 19:20:47 +0700

    Add CountMhs method

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -17,5 +17,31 @@ namespace CRUDMahasiswaADO
         DataTable dtMahasiswa;
         DataTable dtProdi;
 
+        public static string GetConnectionString()
+        {
+            string connectionString = $"Data Source={GetLoacalIPAddress()};Initial Catalog=DBAkademikADO;User ID=sa;Password=anneiszA1.;";
+            return connectionString;
+        }
+        SqlConnection conn = new SqlConnection(GetConnectionString());
+
+        public int CountMhs()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("sp_CountMahasiswa", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter outputParam = new SqlParameter("@Total", SqlDbType.Int);
+            outputParam.Direction = ParameterDirection.Output;
+            cmd.Parameters.Add(outputParam);
+
+            cmd.ExecuteNonQuery();
+
+            return Convert.ToInt32(outputParam.Value);
+        }
+
     }
 }
